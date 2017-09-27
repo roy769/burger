4a72efa2348cc730475bf9231d3447150285dd1f
roy769 <roy@mortgallery.com>
2017-09-27 15:50:39 -0700

    mysql database added

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+drop database if exists burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+
+    id int (16) auto_increment not null,
+    burger_name varchar (32) not null,
+    devoured boolean not null,
+    date timestamp not null,
+    primary key (id)
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Heart-Attack', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Skiiny-Cow',false);
+INSERT INTO burgers (buger_name, devoured) VALUES ('Korean', true);
