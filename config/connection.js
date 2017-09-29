912c2de3dd54279f1d151fca3067f803941ce93d
roy769 <roy@mortgallery.com>
2017-09-28 22:16:20 -0700

    connection cleaned

--- config/connection.js
+++ config/connection.js
@@ -4,10 +4,10 @@ var mysql = require("mysql");
 var connection;
 if (process.env.JAWSDB_URL)
 { connection = mysql.createConnection(process.env.JAWSDB_URL)
-  else
+}  else
   {
-    connection = mysql.createConnection(
-  }    
+    connection = mysql.createConnection({
+
     port: 3306,
     host: "localhost",
     user: "root",
